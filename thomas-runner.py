b3dc1322d17cc0b2e29676c7479f07d4f03e85be
Thomas Øverby <thomas.overby@embriq.no>
2016-03-09 15:59:56 +0100

    test

--- thomas-runner.py
+++ thomas-runner.py
@@ -17,26 +17,30 @@ if __name__ == '__main__':
         print ('Training data loaded!')
         # Initialize camera
         camera = config.get_camera()
-
-        print ('Running detection...')
+        
+		print ('Running detection...')
         print ('Press Ctrl-C to quit.')
         while True:
                 # Check for the positive face and unlock if found.
                 image = camera.read()
+                
                 # Convert image to grayscale.
                 image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
                 # Get coordinates of single face in captured image.
                 result = face.detect_single(image)
                 print (result)
-                cv2.imshow('image',image)
+                
                 if result is None:
                         print ('Could not detect single face!  Check the image in capture.pgm' \
                                ' to see what was captured and try again with only one face visible.')
                         continue
                 
                 x, y, w, h = result
+				cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+				cv2.imshow('image',image)
                 # Crop and resize image to face.
                 crop = face.resize(face.crop(image, x, y, w, h))
                 # Test face against recognizer.
                 predicted = recognizer.predict(crop)
-                print('result from predict is %d: ' % predicted)
+				
+                
