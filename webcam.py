2f4c96cd98dde2e4c20a6e5c37751eea9582ae07
The Pi <pi@embriq.no>
2016-03-09 15:53:39 +0000

    Fixed fucked up git

--- webcam.py
+++ webcam.py
@@ -33,7 +33,7 @@ while True:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
     # Display the resulting frame
-    cv2.imshow('Video', frame)
+    cv2.imshow('image', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
